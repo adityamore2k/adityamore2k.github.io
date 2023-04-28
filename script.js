e75e406053143df8e080704bfff4c5858753bf8d
Aditya <aditya.more@mmit.edu.in>
2023-04-28 11:12:54 +0530

    Education and contact updated

--- script.js
+++ script.js
@@ -1,85 +1,108 @@
-jQuery(document).ready(function() {
-    jQuery('.skillbar').each(function() {
-      jQuery(this).find('.skillbar-bar').animate({
-        width: jQuery(this).attr('data-percent')
-      }, 4000);
-    });
+// typewriter effect
+jQuery(document).ready(function () {
+  jQuery('.skillbar').each(function () {
+    jQuery(this).find('.skillbar-bar').animate({
+      width: jQuery(this).attr('data-percent')
+    }, 4000);
   });
+});
 
-  let i = 0;
-  let target = document.getElementById("anim-typewriter");
-  let text = target.innerHTML;
-  text = text.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&sol;/g, "/");
-  console.log(text);
-  target.innerHTML = ' ';
-  let speed = 75; //speed duration of effect in millisec
+let i = 0;
+let target = document.getElementById("anim-typewriter");
+let text = target.innerHTML;
+text = text.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&sol;/g, "/");
+console.log(text);
+target.innerHTML = ' ';
+let speed = 75; //speed duration of effect in millisec
 
-  typeWriter(); //to call function
-  function typeWriter() {
-      if (i < text.length) {
-          target.innerHTML += text.charAt(i);
-          i++;
-          setTimeout(typeWriter, speed);
-      }
+typeWriter();
+
+function typeWriter() {
+  if (i < text.length) {
+    target.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
+}
 
-  var vsOpts = {
-    slides: document.querySelectorAll('.v-slide'),
-    list: document.querySelector('.v-slides'),
-    duration: 0.3,
-    lineHeight: 50
-  }
-  
-  var vSlide = gsap.timeline({
-    paused: true,
-    repeat: -1
-  });
-  
-  vsOpts.slides.forEach(function(slide, i) {
-    // Create a label
-    let label = "slide" + i;
-    vSlide.add(label);
-    
-    // Move the whole word
-    if(i > 0) {
-      vSlide.to(vsOpts.list, {
-        duration: vsOpts.duration,
-        y: i * -1 * vsOpts.lineHeight
-      }, label);
-  
-      // Move each letter
-      let letters = new SplitText(slide, {type:"chars"}).chars;
-      vSlide.from(letters, {
-        duration: vsOpts.duration,
-        y: 50,
-        stagger: vsOpts.duration / 10
-      }, label);
-      
-      // Add some blank space before the next animation
-      vSlide.to(letters, {
-        duration: vsOpts.duration,
-        y: -50,
-        stagger: vsOpts.duration / 10
-      }, "+=1");
+var container = document.getElementById('projects')
+var slider = document.getElementById('slider');
+var slides = document.getElementsByClassName('slide').length;
+var buttons = document.getElementsByClassName('btn');
+
+
+var currentPosition = 0;
+var currentMargin = 0;
+var slidesPerPage = 0;
+var slidesCount = slides - slidesPerPage;
+var containerWidth = container.offsetWidth;
+var prevKeyActive = false;
+var nextKeyActive = true;
+
+window.addEventListener("resize", checkWidth);
+
+function checkWidth() {
+    containerWidth = container.offsetWidth;
+    setParams(containerWidth);
+}
+
+function setParams(w) {
+    if (w < 551) {
+        slidesPerPage = 1;
+    } else {
+        if (w < 901) {
+            slidesPerPage = 2;
+        } else {
+            if (w < 1101) {
+                slidesPerPage = 3;
+            } else {
+                slidesPerPage = 4;
+            }
+        }
     }
-  })
+    slidesCount = slides - slidesPerPage;
+    if (currentPosition > slidesCount) {
+        currentPosition -= slidesPerPage;
+    };
+    currentMargin = - currentPosition * (100 / slidesPerPage);
+    slider.style.marginLeft = currentMargin + '%';
+    if (currentPosition > 0) {
+        buttons[0].classList.remove('inactive');
+    }
+    if (currentPosition < slidesCount) {
+        buttons[1].classList.remove('inactive');
+    }
+    if (currentPosition >= slidesCount) {
+        buttons[1].classList.add('inactive');
+    }
+}
 
-vSlide.play();
+setParams();
 
-const carouselItems = document.querySelector('.carousel-items');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
+function slideRight() {
+    if (currentPosition != 0) {
+        slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
+        currentMargin += (100 / slidesPerPage);
+        currentPosition--;
+    };
+    if (currentPosition === 0) {
+        buttons[0].classList.add('inactive');
+    }
+    if (currentPosition < slidesCount) {
+        buttons[1].classList.remove('inactive');
+    }
+};
 
-prevButton.addEventListener('click', () => {
-  carouselItems.scrollBy({
-    left: -250,
-    behavior: 'smooth'
-  });
-});
-
-nextButton.addEventListener('click', () => {
-  carouselItems.scrollBy({
-    left: 250,
-    behavior: 'smooth'
-  });
-});
+function slideLeft() {
+    if (currentPosition != slidesCount) {
+        slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
+        currentMargin -= (100 / slidesPerPage);
+        currentPosition++;
+    };
+    if (currentPosition == slidesCount) {
+        buttons[1].classList.add('inactive');
+    }
+    if (currentPosition > 0) {
+        buttons[0].classList.remove('inactive');
+    }
+};
